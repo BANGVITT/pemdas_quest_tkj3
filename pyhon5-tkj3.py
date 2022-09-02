2b02138806fd7634d69b7608c969aa9deb7885aa
BANGVITT <112367918+BANGVITT@users.noreply.github.com>
2022-09-01 20:23:45 -0700

    DAVID EDI SAPUTRA X TKJ 3
    python5

---
+++ pyhon5-tkj3.py
@@ -0,0 +1,65 @@
+#Rumus persegi sisi * sisi
+
+sisil = 10
+sisi2 = 10
+luas_p = sisil * sisi2
+
+print("luas persegi adalah" , luas_p)
+
+
+
+
+
+#Rumus Segitiga  A * T / 2
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi
+
+print("luas segitiga adalah" , luas_s)
+
+
+
+
+
+
+#Rumus Jajar genjang Alas * tinggi
+alas = 10
+tinggi = 20
+luas_j = alas * tinggi
+
+print("luas jajar genjang adalah" , luas_j)
+
+
+
+#Rumus Lingkaran phi * r * r
+phi = 22/7
+r = 21
+luas_l = phi * r * r
+
+
+print("luas lingkaran adalah" , luas_l)
+
+
+
+
+
+print("\ni.luas persegi")
+sisi = int(input("masukan nilai sisi : "))
+luas_p = sisi * sisi
+print("luas persegi adalah" , luas_p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
