4d9df8b9fca2c9bb529658ef16a2e43fd1fbe70f
BANGVITT <112367918+BANGVITT@users.noreply.github.com>
2022-09-06 00:05:03 -0700

    TUGAS PEMDAS DAVID EDI SAPUTRA
    X TKJ 3

---
+++ python6-tkj3.py
@@ -0,0 +1,23 @@
+# Dolar Ke Rupiah
+dolar = int(input("masukan nilai dolar: "))
+rupiah = dolar * 15000
+
+print  ("Dolar Ke Rupiah" , rupiah)
+
+
+
+# Euro Ke Won
+euro = int(input("masukan nilai euro: "))
+won = euro * 1357
+
+print ("Euro Ke Won" , won )
+
+
+
+
+# YEN Ke Rupiah
+yen = int(input("masukan nilai yen: "))
+rupiah = yen * 106
+
+
+print  ("Yen Ke Rupiah" , rupiah)
